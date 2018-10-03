a8072d87e53699932e90f6b3af58cb6e859bbfab
Haotian9850 <haotian9850@gmail.com>
2018-10-02 21:50:10 -0400

    10-02-2018

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -8,12 +8,17 @@
 #include <unordered_map>
 using namespace std;
 
-unordered_map<int, int> dpTable = {{0, 0}}, {1, 1}};    //first & second base case
-
-int solve(int n){
-    if(dpTable.count(n) == 1){
-        
+int fibonacci(int n){
+    int dp[n];
+    //base cases
+    dp[0] = 0;
+    dp[1] = 1;
+    for(int i = 2; i <= n; i ++){
+        dp[i] = dp[i - 2] + dp[i - 1];
     }
-
+    return dp[n];
 }
 
+int main(){
+    cout << fibonacci(29) << endl;
+}
